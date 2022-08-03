ab01de788fcfa63aa13714851205f4c16c603066
KuuLotus <kuulotusy@gmail.com>
2022-08-03 23:28:11 +0900

    [finish]react-bootstrap

---
+++ src/components/BlogPage.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nihil
+        molestias ipsum est maxime. Repellat fugiat incidunt aspernatur nesciunt
+        dolores? Neque enim at molestias maiores! Odio possimus impedit eos
+        nobis nesciunt natus minima. Facere libero error at, magni ipsam et a
+        ipsa repellendus possimus officiis optio animi atque numquam ab
+        voluptatem molestiae obcaecati dolores dolore, alias inventore minus in
+        est, cupiditate doloremque? Unde velit exercitationem hic dignissimos
+        quod rem explicabo consequuntur nobis delectus reprehenderit iste magni
+        laborum minima a deleniti quisquam, expedita eum cumque in! Mollitia,
+        tempore voluptatem quos reiciendis asperiores repellat praesentium
+        recusandae, expedita suscipit laboriosam est maxime fugiat?
+      </p>
+    </div>
+  );
+};
+
+export default BlogPage;
